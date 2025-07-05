44d5dfddd37faced2fa25b46858b9d90155bfd58
rubilnik4 <rubilnik4@yandex.ru>
2025-07-05 18:48:55 +0500

    fix: optimize imports

--- witch-please/src/main/scala/tarot/domain/models/cards/Card.scala
+++ witch-please/src/main/scala/tarot/domain/models/cards/Card.scala
@@ -1,21 +1,19 @@
 package tarot.domain.models.cards
 
 import tarot.domain.models.contracts.SpreadId
-import tarot.domain.models.photo.{PhotoFile, PhotoOwnerType, Photo, PhotoSource}
-import tarot.domain.models.spreads.Card
+import tarot.domain.models.photo.*
 import tarot.infrastructure.services.common.DateTimeService
-import zio.json.{DeriveJsonCodec, JsonCodec}
-import zio.schema.{DeriveSchema, Schema}
 
 import java.time.Instant
 import java.util.UUID
 
 final case class Card(
-                       id: UUID,
-                       spreadId: SpreadId,
-                       description: String,
-                       coverPhoto: Photo,
-                       time: Instant)
+  id: UUID,
+  spreadId: SpreadId,
+  description: String,
+  coverPhoto: Photo,
+  time: Instant
+)
 {
   override def toString: String = s"card id: $id; spreadId:$spreadId"
 }
