30a35b64dd873bcdb61ff935164ccc19e25a44df
rubilnik4 <rubilnik4@yandex.ru>
2025-11-25 13:39:18 +0500

    fix: remove project links

--- witch-please/bot/src/main/scala/bot/application/handlers/telegram/TelegramCommandHandler.scala
+++ witch-please/bot/src/main/scala/bot/application/handlers/telegram/TelegramCommandHandler.scala
@@ -1,13 +1,7 @@
 package bot.application.handlers.telegram
 
-import bot.api.dto.TelegramWebhookRequest
-import bot.application.commands.BotCommand
 import bot.domain.models.telegram.*
-import bot.infrastructure.services.authorize.SecretService
 import bot.layers.BotEnv
-import shared.api.dto.tarot.projects.*
-import shared.api.dto.tarot.spreads.*
-import shared.api.dto.tarot.users.*
 import zio.ZIO
 
 trait TelegramCommandHandler() {
