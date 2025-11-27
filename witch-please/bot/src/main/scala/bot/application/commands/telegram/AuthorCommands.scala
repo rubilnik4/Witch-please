481ffe09d3ff6955ed3f35c61369c93e21765f95
rubilnik4 <rubilnik4@yandex.ru>
2025-11-27 11:11:01 +0500

    fix: remove / in bot prefix

--- witch-please/bot/src/main/scala/bot/application/commands/telegram/AuthorCommands.scala
+++ witch-please/bot/src/main/scala/bot/application/commands/telegram/AuthorCommands.scala
@@ -4,13 +4,13 @@ import java.util.UUID
 
 object AuthorCommands {
   final val Prefix = "/author"
-  final val Start = s"/${Prefix}_start"
-  final val SpreadCreate = s"/${Prefix}_spread_create"
-  final val SpreadSelect = s"/${Prefix}_spread_select"
-  final val SpreadCardsSelect = s"/${Prefix}_spread_cards_select"
-  final val SpreadPublish = s"/${Prefix}_spread_publish"
-  final val SpreadDelete = s"/${Prefix}_spread_delete"
-  final val CardCreate = s"/${Prefix}_card_create"
+  final val Start = s"${Prefix}_start"
+  final val SpreadCreate = s"${Prefix}_spread_create"
+  final val SpreadSelect = s"${Prefix}_spread_select"
+  final val SpreadCardsSelect = s"${Prefix}_spread_cards_select"
+  final val SpreadPublish = s"${Prefix}_spread_publish"
+  final val SpreadDelete = s"${Prefix}_spread_delete"
+  final val CardCreate = s"${Prefix}_card_create"
 
   def spreadSelect(spreadId: UUID, cardCount: Int): String =
     s"$SpreadSelect $spreadId $cardCount"
--- witch-please/bot/src/main/scala/bot/application/commands/telegram/ClientCommands.scala
+++ witch-please/bot/src/main/scala/bot/application/commands/telegram/ClientCommands.scala
@@ -4,8 +4,8 @@ import java.util.UUID
 
 object ClientCommands {
   final val Prefix = "/client"
-  final val Start = s"/${Prefix}_start"
-  final val AuthorSelect = s"/${Prefix}_author_select"
+  final val Start = s"${Prefix}_start"
+  final val AuthorSelect = s"${Prefix}_author_select"
 
   def authorSelect(authorId: UUID): String =
     s"$AuthorSelect $authorId"
