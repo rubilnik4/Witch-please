90297440420d01488435255a193a79fb103e94ac
rubilnik4 <rubilnik4@yandex.ru>
2026-03-23 10:53:01 +0500

    refactor: add develop config

--- witch-please/bot/src/main/scala/bot/layers/BotConfigLayer.scala
+++ witch-please/bot/src/main/scala/bot/layers/BotConfigLayer.scala
@@ -1,15 +1,30 @@
 package bot.layers
 
 import bot.application.configurations.BotConfig
+import com.typesafe.config.ConfigFactory
 import zio.*
 import zio.config.*
 import zio.config.typesafe.TypesafeConfigProvider
 
 object BotConfigLayer {
   val configLive: ZLayer[Any, Config.Error, BotConfig] = {
-    val provider = TypesafeConfigProvider.fromResourcePath()
+    val provider = TypesafeConfigProvider.fromTypesafeConfig(loadConfig())
     ZLayer.fromZIO(
       read(BotConfig.config from provider)
     )
   }
+
+  private def loadConfig() = {
+    val baseConfig = ConfigFactory.parseResources("application.conf")
+    val profileConfig = currentProfile
+      .filter(_.nonEmpty)
+      .map(profile => ConfigFactory.parseResources(s"application.$profile.conf"))
+      .getOrElse(ConfigFactory.empty())
+
+    profileConfig.withFallback(baseConfig).resolve()
+  }
+
+  private def currentProfile: Option[String] =
+    sys.env.get("APP_ENV")
+      .map(_.trim.toLowerCase)
 }
--- witch-please/core/src/main/scala/tarot/layers/TarotConfigLayer.scala
+++ witch-please/core/src/main/scala/tarot/layers/TarotConfigLayer.scala
@@ -1,5 +1,6 @@
 package tarot.layers
 
+import com.typesafe.config.ConfigFactory
 import tarot.application.configurations.TarotConfig
 import zio.*
 import zio.config.*
@@ -7,9 +8,23 @@ import zio.config.typesafe.TypesafeConfigProvider
 
 object TarotConfigLayer {
   val appConfigLive: ZLayer[Any, Config.Error, TarotConfig] = {
-    val provider = TypesafeConfigProvider.fromResourcePath()
+    val provider = TypesafeConfigProvider.fromTypesafeConfig(loadConfig())
     ZLayer.fromZIO(
       read(TarotConfig.config from provider)
     )
   }
+
+  private def loadConfig() = {
+    val baseConfig = ConfigFactory.parseResources("application.conf")
+    val profileConfig = currentProfile
+      .filter(_.nonEmpty)
+      .map(profile => ConfigFactory.parseResources(s"application.$profile.conf"))
+      .getOrElse(ConfigFactory.empty())
+
+    profileConfig.withFallback(baseConfig).resolve()
+  }
+
+  private def currentProfile: Option[String] =
+    sys.env.get("APP_ENV")
+      .map(_.trim.toLowerCase)
 }
